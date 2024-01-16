621df79fc452b19881e9ee0e6499574ea2ce6ad6
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-01-16 18:54:26 -0500

    Update blank-scripts.js

--- assets/js/blank-scripts.js
+++ assets/js/blank-scripts.js
@@ -1,10 +1,3 @@
-document.onload = windowHiddenAlert();
-
-function windowHiddenAlert() { 
-
 var frame = document.getElementById("ijctfrm").contentWindow;
-
 frame.postMessage("jc<3mb", "*")
-  
-} 
 
