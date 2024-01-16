592d969983a9c8ce41fb602776020fc5ef26236e
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-01-16 18:48:19 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -141,7 +141,7 @@
 	var gameToLaunchN = gameTile.getAttribute("data-game-normal")
 	console.log("game is " + gameToLaunch)
 	
-	document.getElementById("gfgdpl").src = 'https://' + location.hostname + '/games/' + '/holders/' + gameToLaunch;
+	document.getElementById("gfgdpl").src = 'https://' + location.hostname + '/games' + '/holders/' + gameToLaunch;
 	document.getElementById("gnihtm").innerHTML = 'CLICK TO RESUME ' + gameToLaunchN;
 	document.getElementById("ignhr").innerHTML = gameToLaunchN;
 	
